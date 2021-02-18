83ad27a7ca0c01a838b433ec5ae7e5833ec95c65
Brandon <brandon@tendency.me>
2021-02-18 04:48:52 -0600

    v1.0.3 (#4)
    * chore: Add compatibility with Acorn v2.0.0
    * chore(ci): Move to GitHub actions
    * chore(git): Update .gitignore and .gitattributes
    * chore(deps): Bump dependencies

--- src/Console/FormMakeCommand.php
+++ src/Console/FormMakeCommand.php
@@ -123,6 +123,54 @@ class FormMakeCommand extends GeneratorCommand
     }
 
     /**
+     * Clear the current line in console output.
+     *
+     * @return Command
+     */
+    public function clearLine()
+    {
+        if (! $this->output->isDecorated()) {
+            $this->output->writeln('');
+
+            return $this;
+        }
+
+        $this->output->write("\x0D");
+        $this->output->write("\x1B[2K");
+
+        return $this;
+    }
+
+    /**
+     * Run a task in the console.
+     *
+     * @param  string        $title
+     * @param  callable|null $task
+     * @param  string        $status
+     * @return mixed
+     */
+    protected function task($title, $task = null, $status = '...')
+    {
+        $title = Str::start($title, '<fg=blue;options=bold>➡</> ');
+
+        if (! $task) {
+            return $this->output->write("{$title}: <info>✔</info>");
+        }
+
+        $this->output->write("{$title}: <comment>{$status}</comment>");
+
+        try {
+            $status = $task() !== false;
+        } catch (\Exception $e) {
+            $this->clearLine()->line("{$title}: <fg=red;options=bold>x</>");
+
+            throw $e;
+        }
+
+        $this->clearLine()->line("{$title}: " . ($status ? '<info>✔</info>' : '<fg=red;options=bold>x</>'));
+    }
+
+    /**
      * Returns a shortened path.
      *
      * @param  string $path
